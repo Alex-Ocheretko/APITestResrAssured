478d3f9df2abd49b626c5ad4ef3fd0bdef08687c
Alex-Ocheretko <olexandr.o.ya@gmail.com>
2021-02-18 14:41:34 +0200

    Completed get100RandomUsers test

--- src/test/java/test/Test1.java
+++ src/test/java/test/Test1.java
@@ -1,25 +1,40 @@
+package test;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.testng.annotations.*;
-import pageObject.GoogleSearchPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import test.pageObject.DriverWrapper;
+import test.pageObject.GoogleSearchPage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
+
 
 import static io.restassured.RestAssured.given;
 
 public class Test1 {
 
-    DriverWrapper driverWrapper;
+//    DriverWrapper driverWrapper = new DriverWrapper();
+
     GoogleSearchPage googleSearchPage;
 
+
     @Test
-    public void get100RandomUser() {
+    public void get100RandomUsers() {
 
         int numberOfFemale = 0;
         int numberOfMale = 0;
+        StringBuilder sb = new StringBuilder();
+        sb.append("first name");
+        sb.append(",");
+        sb.append("last name");
+        sb.append("\n");
 
         Response response = doGetRequest("https://randomuser.me/api/?inc=gender,nat,name&exc=nat&results=100");
         JsonPath path = response.jsonPath();
@@ -27,16 +42,39 @@ public class Test1 {
 
         for (HashMap<String, Object> singleObject : results) {
             Map<String, Object> name = (Map<String, Object>) singleObject.get("name");
-            System.out.println(name.get("first") + " " + name.get("last"));
             if ("female".equals(singleObject.get("gender"))) {
                 numberOfFemale += 1;
             }
             if ("male".equals(singleObject.get("gender"))) {
                 numberOfMale += 1;
             }
+            try (PrintWriter writer = new PrintWriter(new File(System.getProperty("user.dir") + "/src/main/resources/test.csv"))) {
+
+                sb.append(name.get("first"));
+                sb.append(",");
+                sb.append(name.get("last"));
+                sb.append("\n");
+
+                writer.write(sb.toString());
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println("Female: " + numberOfFemale + " Male: " + numberOfMale);
+
+        Assert.assertEquals(numberOfFemale + numberOfMale, 100);
     }
+
+
+//    @Test
+//    void searchUsersInFB() {
+//        driverWrapper.init();
+//        googleSearchPage.clickOnSearchLine();
+//        googleSearchPage.searchInGoogle("Kolya FB");
+//        driverWrapper.close();
+//        Assert.assertEquals(1,1);
+//    }
 
     public static Response doGetRequest(String endpoint) {
         RestAssured.defaultParser = Parser.JSON;
